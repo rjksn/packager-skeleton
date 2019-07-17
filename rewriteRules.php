cf0563ba1cc279947e8e32186d13336b6f9c33ee
Ryan Jackson <ryan@rjackson.ca>
2019-07-17 15:10:35 -0400

    wrong duplicate

--- rewriteRules.php
+++ rewriteRules.php
@@ -5,6 +5,5 @@ return [
     'src/Services/MyPackage.php' => 'src/Services/:uc:package.php',
     'src/Facades/MyPackage.php' => 'src/Facades/:uc:package.php',
     'src/Providers/MyPackageServiceProvider.php' => 'src/Providers/:uc:packageServiceProvider.php',
-
     'config/mypackage.php' => 'config/:lc:package.php',
 ];
